e662efc6adceb432b201e6e774d3b8ab4f01f415
nik <kolxz@mail.ru>
2022-01-19 13:53:10 +0300

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+// import 'package:qr_code/pages/login.dart';
+import 'package:qr_code/pages/licenseAgreement.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home();
-
-))
+  home: LicenseAgreement(),
+));
 
---
+++ lib/pages/licenseAgreement.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LicenseAgreement extends StatefulWidget {
+  const LicenseAgreement({Key? key}) : super(key: key);
+
+  @override
+  State<LicenseAgreement> createState() => _LicenseAgreementState();
+}
+
+class _LicenseAgreementState extends State<LicenseAgreement> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView(
+                children: const [
+                  Text("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
+                ],
+              )
+          ),
+        ],
+      )
+    );
+  }
+}
