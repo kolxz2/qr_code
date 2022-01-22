6ad5a4a4cf9b87a2c30be918f5cd97b950d7c05d
nik <kolxz@mail.ru>
2022-01-22 15:05:20 +0300

    Don't work PATH

--- lib/pages/qrCode.dart
+++ lib/pages/qrCode.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:dio/dio.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:open_file/open_file.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path/path.dart' as path;
 
 class QrCode extends StatefulWidget {
   const QrCode({Key? key}) : super(key: key);
@@ -15,23 +19,68 @@ class QrCode extends StatefulWidget {
 
 class _QrCodeState extends State<QrCode> {
 
-  final String _url = 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg';
-  final String _image = 'Gull_portrait_ca_usa.jpg';
+  final String _fileUrl = 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg';
+  final String _fileName = 'Gull_portrait_ca_usa.jpg';
+  final Dio _dio = Dio();
 
-  _download() async {
-    // download
+  String _progress = "-";
+
+
+  Future<bool> _requestPermissions() async {
+    var permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+
+    if (permission != PermissionStatus.granted) {
+      await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+      permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+    }
+
+    return permission == PermissionStatus.granted;
   }
 
-  Future<Directory> _getDownloadDirectory() async {
-    if (Platform.isAndroid) {
-      return await DownloadsPathProvider.downloadsDirectory;
+  void _onReceiveProgress(int received, int total) {
+    if (total != -1) {
+      setState(() {
+        _progress = (received / total * 100).toStringAsFixed(0) + "%";
+      });
     }
-    // in this example we are using only Android and iOS so I can assume
-    // that you are not trying it for other platforms and the if statement
-    // for iOS is unnecessary
+  }
 
-    // iOS directory visible to user
+  Future<void> _startDownload(String savePath) async {
+    Map<String, dynamic> result = {
+      'isSuccess': false,
+      'filePath': null,
+      'error': null,
+    };
+    try {
+      final response = await _dio.download(
+          _fileUrl,
+          savePath,
+          onReceiveProgress: _onReceiveProgress
+      );
+      result['isSuccess'] = response.statusCode == 200;
+      result['filePath'] = savePath;
+    } catch (ex) {
+      result['error'] = ex.toString();
+    }
+  }
+
+  Future<Directory?> _getDownloadDirectory() async {
+    if (Platform.isAndroid) {
+      return await DownloadsPathProvider.downloadsDirectory; DownloadsPathProvider.downloadsDirectory;
+    }
     return await getApplicationDocumentsDirectory();
+  }
+
+  Future<void> _download() async {
+    final dir = await _getDownloadDirectory();
+    final isPermissionStatusGranted = await _requestPermissions();
+
+    if (isPermissionStatusGranted) {
+      final savePath = path.join(dir.path, _fileName);
+      await _startDownload(savePath);
+    } else {
+      // handle the scenario when user declines the permissions
+    }
   }
 
   @override
@@ -53,7 +102,7 @@ class _QrCodeState extends State<QrCode> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       ElevatedButton(
-                        onPressed: _download(),
+                        onPressed: _download,
                         child: const Text("Скачать",
                           style: TextStyle(
                             fontSize: 30,
